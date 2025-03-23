33d94e76f4c82ac6498ed81e95c163633d4b35b8
Pratik33 <work.pratikmothe@gmail.com>
2025-03-23 13:48:39 +0530

    refact : submission

--- cactro-test/src/app/app.component.ts
+++ cactro-test/src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { CardComponent } from './component/card/card.component';
+import { MessageBarComponent } from './component/message-bar/message-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CardComponent, MessageBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cactro-test';
+
+  leftCount = 0;
+  rightCount = 0;
+  topCount = 0;
+  visitedCount = 0;
+
+  handleClick(position: string) {
+    switch (position) {
+      case 'left': {
+        this.leftCount = this.leftCount + 1;
+        return;
+      }
+      case 'right': {
+        this.rightCount = this.rightCount + 1;
+        return;
+      }
+      case 'top': {
+        this.topCount = this.topCount + 1;
+        return;
+      }
+      case 'visited': {
+        this.visitedCount = this.visitedCount + 1;
+        return;
+      }
+    }
+  }
 }
---
+++ cactro-test/src/app/component/message-bar/message-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-bar',
+  imports: [],
+  templateUrl: './message-bar.component.html',
+  styleUrl: './message-bar.component.scss'
+})
+export class MessageBarComponent {
+
+}
